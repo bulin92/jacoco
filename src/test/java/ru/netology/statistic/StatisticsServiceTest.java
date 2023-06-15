f66c4325616197868bf6b6be22fe6cfd4a8ed497
Bulin Alexey <bulin92@inbox.ru>
2023-06-15 23:56:00 +0700

    testHundreedPercent

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxSecond() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 25, 8, 6, 11, 11, 12};
+        long expected = 25;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
